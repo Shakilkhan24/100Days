86e44ba6a02bab230236c571b4589a5090c588c4
1y33 <catalin.milu.2004@gmail.com>
2025-01-28 20:52:45 +0200

    updated

--- day13/RMSBetter.cu
+++ day13/RMSBetter.cu
@@ -1,6 +1,5 @@
 #include <cuda_runtime.h>
 
-
 #define CEILING(x, y) (((x) + (y) - 1) / (y))
 
 #define blockdimy 128
@@ -16,22 +15,25 @@ __device__ float warpReduceSum(float val)
 
 __global__ void RMSKernel_V2(float *input, float *output, const int w, const int h)
 {
-    int row = blockIdx.x * blockDim.x + threadIdx.x; 
-    int col = blockIdx.y * blockDim.y + threadIdx.y; 
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-    __shared__ float shared_data[32];  
+    __shared__ float shared_data[32];
 
     float sum = 0.0f;
 
     if (row < h && col < w)
     {
-        float4 val = reinterpret_cast<float4*>(&input[row * w + col * 4])[0]; 
-        sum += val.x * val.x + val.y * val.y + val.z * val.z + val.w * val.w; 
+        float4 val = reinterpret_cast<float4 *>(&input[row * w + col * 4])[0];
+        sum += val.x * val.x + val.y * val.y + val.z * val.z + val.w * val.w;
     }
+    __syncthreads();
 
     sum = warpReduceSum(sum);
 
-    if (threadIdx.x % 32 == 0) 
+    __syncthreads();
+
+    if (threadIdx.x % 32 == 0)
     {
         shared_data[threadIdx.x / 32] = sum;
     }
@@ -41,18 +43,17 @@ __global__ void RMSKernel_V2(float *input, float *output, const int w, const int
     if (threadIdx.x == 0)
     {
         float final_sum = 0.0f;
-        for (int i = 0; i < blockDim.x / 32; ++i) {
+        for (int i = 0; i < blockDim.x / 32; ++i)
+        {
             final_sum += shared_data[i];
         }
-
-        output[row] = sqrt(final_sum / (w * h));
+        output[row] = input[row] / sqrt(final_sum / float(w));
     }
 }
 
-
 void RMSV2(float *input, float *output, int w, int h)
 {
-    dim3 block_size = dim3(1, 32, 1);  
-    dim3 grid_size = dim3(h, 1, 1);    
+    dim3 block_size = dim3(1, 32, 1);
+    dim3 grid_size = dim3(h, 1, 1);
     RMSKernel_V2<<<grid_size, block_size>>>(input, output, w, h);
 }
